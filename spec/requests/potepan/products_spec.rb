569ccd89b6f096179ca15aa4dde0e035d928f2ac
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-09-18 10:22:58 +0900

    potepan/prpducts/show.html.erbへのリクエストスペックの導入

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,9 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET /show" do
-    it "returns http success" do
-      get "/potepan/products/show"
+    let!(:product) { create(:product) }
+
+    before do
+      get potepan_product_url(product.id)
+    end
+
+    it "レスポンスを返すこと" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "ビューを返すこと" do
+      expect(response).to render_template(:show)
+    end
+
+    it "商品が表示されていること" do
+      expect(response.body).to include product.name
+      expect(response.body).to include product.display_price.to_s
+      expect(response.body).to include product.description
+    end
+
+    it "コントローラー作成の@prpductとテスト作成のproductが一致していること" do
+      expect(assigns(:product)).to eq product
     end
   end
 end
