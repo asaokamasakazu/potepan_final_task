c5c15649df278dfdc23c6107eba135febfc655d2
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-09-19 12:07:12 +0900

    [update]letのエクスクラメーションマークを削除
    #5 beforeによってletが使用されるため削除しました。

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET /show" do
-    let!(:product) { create(:product) }
+    let(:product) { create(:product) }
 
     before do
       get potepan_product_url(product.id)
