3cfefb58ba724db547cc1ad4d068d8d5501efa20
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-04 10:06:23 +0900

    taxonに紐づくproductだけ取得できているか検証するテストと、サイドバーのカテゴリーに商品数が表示されているか検証するテストを追加

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 RSpec.describe "Potepan::Categories", type: :request do
   describe "GET /show" do
     let(:taxonomy) { create(:taxonomy) }
-    let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
-    let(:product) { create(:product, taxons: [taxon]) }
-    let(:image) { create(:image) }
+    let(:taxon) { create(:taxon, name: "Tops", taxonomy: taxonomy, parent: taxonomy.root) }
+    let(:other_taxon) { create(:taxon, name: "Pants", taxonomy: taxonomy, parent: taxonomy.root) }
+    let(:product1) { create(:product, name: "Rails Shirts", taxons: [taxon]) }
+    let(:product2) { create(:product, name: "Solidus Coat", taxons: [taxon]) }
+    let(:other_product) { create(:product, name: "Rails Denim", taxons: [other_taxon]) }
+    let(:image1) { create(:image) }
+    let(:image2) { create(:image) }
+    let(:other_image) { create(:image) }
 
     before do
-      product.images << image
+      product1.images << image1
+      product2.images << image2
+      other_product.images << other_image
       get potepan_category_path(taxon.id)
     end
 
@@ -16,17 +23,31 @@ RSpec.describe "Potepan::Categories", type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it "商品情報が表示されていること" do
-      expect(response.body).to include product.name
-      expect(response.body).to include product.display_price.to_s
+    it "taxonに紐づくproductだけ表示されていること" do
+      expect(response.body).to include product1.name
+      expect(response.body).to include product2.name
+      expect(response.body).not_to include other_product.name
     end
 
-    it "商品カテゴリとしてtaxonomy.nameが表示されていること" do
+    it "商品価格が表示されていること" do
+      expect(response.body).to include product1.display_price.to_s
+      expect(response.body).to include product2.display_price.to_s
+    end
+
+    it "商品カテゴリーとしてtaxonomy.nameが表示されていること" do
       expect(response.body).to include taxonomy.name
     end
 
-    it "商品カテゴリとしてtaxon.nameが表示されていること" do
+    it "商品カテゴリーとしてtaxon.nameとother_taxon.nameが表示されていること" do
       expect(response.body).to include taxon.name
+      expect(response.body).to include other_taxon.name
+    end
+
+    it "カテゴリーに商品数が表示されていること" do
+      within ".side-nav" do
+        expect(response.body).to include taxon.products.count.to_s
+        expect(response.body).to include other_taxon.products.count.to_s
+      end
     end
   end
 end
