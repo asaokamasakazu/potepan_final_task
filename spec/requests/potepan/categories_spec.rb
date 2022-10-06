9a1240069efca91e4790d39b9ca5610ba305c8f4
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-06 10:12:50 +0900

    不要なテストデータの削除
    product2がなくてもテストを行えるため削除しました。

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -5,16 +5,13 @@ RSpec.describe "Potepan::Categories", type: :request do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
     let(:other_taxon) { create(:taxon, name: "Pants", taxonomy: taxonomy, parent: taxonomy.root) }
-    let(:product1) { create(:product, taxons: [taxon]) }
-    let(:product2) { create(:product, taxons: [taxon]) }
+    let(:product) { create(:product, taxons: [taxon]) }
     let(:other_product) { create(:product, taxons: [other_taxon]) }
-    let(:image1) { create(:image) }
-    let(:image2) { create(:image) }
+    let(:image) { create(:image) }
     let(:other_image) { create(:image) }
 
     before do
-      product1.images << image1
-      product2.images << image2
+      product.images << image
       other_product.images << other_image
       get potepan_category_path(taxon.id)
     end
@@ -25,14 +22,12 @@ RSpec.describe "Potepan::Categories", type: :request do
     end
 
     it "taxonに紐づくproductだけを返すこと" do
-      expect(response.body).to include product1.name
-      expect(response.body).to include product2.name
+      expect(response.body).to include product.name
       expect(response.body).not_to include other_product.name
     end
 
     it "商品価格を返すこと" do
-      expect(response.body).to include product1.display_price.to_s
-      expect(response.body).to include product2.display_price.to_s
+      expect(response.body).to include product.display_price.to_s
     end
 
     it "商品カテゴリーとしてtaxonomy.nameを返すこと" do
