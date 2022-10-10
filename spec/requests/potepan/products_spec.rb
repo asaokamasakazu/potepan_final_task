344f268fb183f6174c5457147d58ce7e9f66d0eb
m.asaoka <asaokamasakazu@yahoo.co.jp>
2022-10-10 15:08:05 +0900

    関連商品部分のrequest specを作成

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe "Potepan::Products", type: :request do
   describe "GET /show" do
     let(:taxon) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon]) }
+    let(:related_products) do
+      5.times.collect do
+        create(:product, price: "#{rand(1.00..99.99)}", taxons: [taxon])
+      end
+    end
+    let(:related_images) { create_list(:image, 5) }
 
     before do
+      related_products.zip(related_images) do |related_product, related_image|
+        related_product.images << related_image
+      end
       get potepan_product_path(product.id)
     end
 
-    it "200レスポンスを返すこと" do
+    it "正常なレスポンスを返すこと" do
       expect(response).to be_successful
       expect(response).to have_http_status "200"
     end
@@ -18,6 +27,15 @@ RSpec.describe "Potepan::Products", type: :request do
       expect(response.body).to include product.name
       expect(response.body).to include product.display_price.to_s
       expect(response.body).to include product.description
+    end
+
+    it "関連商品を4つのみ取得していること" do
+      related_products.first(4).all? do |related_product|
+        expect(response.body).to include related_product.name
+        expect(response.body).to include related_product.display_price.to_s
+      end
+      expect(response.body).not_to include related_products.last.name
+      expect(response.body).not_to include related_products.last.display_price.to_s
     end
   end
 end
